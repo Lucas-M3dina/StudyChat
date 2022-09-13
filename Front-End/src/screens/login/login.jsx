179f0ea25476067a74698989fac855f16f6c0b92
ferreirawill05 <william.ferreira@outlook.com.br>
2022-09-13 13:48:15 -0300

    home-login-cadastro

--- Front-End/src/screens/login/login.jsx
+++ Front-End/src/screens/login/login.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import logo from "../../assets/logo.png"
 import fundo from "../../assets/banner-cadastro-fundo.png"
 import css from "./login_style.css"
-
 import api from '../../services/api'
 import { Component } from 'react';
 import { Link } from 'react-router-dom';
@@ -10,71 +9,76 @@ import { parseJWT, usuarioAutenticacao } from '../../services/auth';
 import { render } from "@testing-library/react";
 
 export default class Login extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        email: '',
-        senha: '',
-        erroMensagem: '',
-        isLoading: false
-      }
-    };
-  
-    efetuaLogin = (event) => {
-      event.preventDefault();
-  
-      this.setState({ erroMensagem: "", isLoading: true })
-  
-      api.post('/api/Login', {
-        email: this.state.email,
-        senha: this.state.senha
-      })
-  
-        .then(resposta => {
-          if (resposta.status === 200) {
-            localStorage.setItem('usuario-login', resposta.data.token);
-            this.setState({ isLoading: false });
-  
-            switch (parseJWT().role) {
-              case "1":
-                this.props.history.push("/chat")
-                console.log("estou logado: " + usuarioAutenticacao())
-                break;
-  
-              case "2":
-                this.props.history.push("/chat")
-                console.log("estou logado: " + usuarioAutenticacao())
-                break;
-  
-              default:
-                this.props.history.push("/chat") //alterar a coisinha aqui
-                break;
-            }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      senha: '',
+      erroMensagem: '',
+      isLoading: false
+    }
+  };
+
+  efetuaLogin = (event) => {
+    event.preventDefault();
+
+    this.setState({ erroMensagem: "", isLoading: true })
+
+    api.post('/api/Login', {
+      email: this.state.email,
+      senha: this.state.senha
+    })
+
+      .then(resposta => {
+        if (resposta.status === 200) {
+          localStorage.setItem('usuario-login', resposta.data.token);
+          this.setState({ isLoading: false });
+
+          switch (parseJWT().role) {
+            case "1":
+              this.props.history.push("/chat")
+              console.log("estou logado: " + usuarioAutenticacao())
+              break;
+
+            case "2":
+              this.props.history.push("/chat")
+              console.log("estou logado: " + usuarioAutenticacao())
+              break;
+
+            default:
+              this.props.history.push("/chat") //alterar a coisinha aqui
+              break;
           }
-        })
-        .catch(() => {
-          this.setState({ erroMensagem: "Email e/ou senha inválidos!", isLoading: false });
-        })
-    }
-  
-    atualizaStateCampo = (campo) => {
-      this.setState({ [campo.target.name]: campo.target.value })
-      console.log([campo.target.name] + ' : ' + campo.target.value)
-    }
+        }
+      })
+      .catch(() => {
+        this.setState({ erroMensagem: "Email e/ou senha inválidos!", isLoading: false });
+      })
+  }
+
+  atualizaStateCampo = (campo) => {
+    this.setState({ [campo.target.name]: campo.target.value })
+    console.log([campo.target.name] + ' : ' + campo.target.value)
+  }
 
 
-render() 
-
-{
+  render() {
     return (
       <div className="box-bodyL">
         <div className="esquerdaL">
-          <img src={logo} alt="logo Darede" />
-          {/* <img src={banner} className="undrawL" alt="banner cadastro" /> */}
+          <div className="txt-left">
+          <img className="logo" src={logo} alt="logo" />
+          <h1>Seu parceiro de estudos!</h1>
+
+          <p>Otimize sua rotina de estudos e faça testes ainda mais intuitivos, utiizando uma nova maneira de pensar com nossos serviços.
+          </p>
+          <p>Acesse nossa plataforma e explore os  limites do seu conhecimento, entre na sua sala e mostre quem manda!
+          </p>
+          </div>
         </div>
         <div className="direitaL">
-          <h1 className='h1-login'>Login</h1>
           <form action="submit" onSubmit={this.efetuaLogin}>
+            <h1 className='h1-login'>Login</h1>
 
             <input type="email" placeholder="Email"
               name="email"
@@ -94,7 +98,7 @@ render()
             }
             {
               this.state.isLoading === false &&
-              <button disabled={this.state.email === '' || this.state.senha === '' ? 'none' : ''} className="btn-formL" type="submit">Login</button>
+              <button disabled={this.state.email === '' || this.state.senha === '' ? 'none' : ''} className="btn-formL" type="submit">Entrar</button>
             }
           </form>
 
