ee3696b5bec21e2366de709b4afc2b1a64d9427e
William Nunes Ferreira <william.ferreira@outlook.com.br>
2022-10-03 10:16:55 -0300

    front-update

--- Front-End/src/screens/chat-mock/teste.jsx
+++ Front-End/src/screens/chat-mock/teste.jsx
@@ -14,6 +14,7 @@ class Review extends Component {
       materia: '',
       sentimento: '',
       questionarios: [],
+      test: [],
     };
   }
 
@@ -158,7 +159,6 @@ class SimpleForm extends Component {
               } else if (value > 120) {
                 return `${value}? Come on!`;
               }
-
               return true;
             },
           },
